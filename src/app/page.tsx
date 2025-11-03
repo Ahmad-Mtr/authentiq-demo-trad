de422f8e8bebf30796cc62c1326627da55e2ba95
Ahmad Karajah <70106976+Ahmad-Mtr@users.noreply.github.com>
2025-11-03 21:18:07 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <AuthGuard requireAuth>
+    /*<AuthGuard requireAuth>*/
       <ProfileContainer />
-    </AuthGuard>
+   /* </AuthGuard>*/
   );
 }
